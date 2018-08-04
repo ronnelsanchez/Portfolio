3c97f2247d94664a342e506cc4d9e165ec9e70a0
ronnelsanchez <ronnel.sanchez@gmail.com>
2018-08-04 15:01:22 +0800

    Update

---
+++ dist/precache-manifest.ef26143e78e80b0a0ec47fb7e65598bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d24b25d7a74904c56039",
+    "url": "/js/chunk-vendors.d24b25d7.js"
+  },
+  {
+    "revision": "2da49b1b4d13fdb9a769",
+    "url": "/js/app.2da49b1b.js"
+  },
+  {
+    "revision": "f20e1495134b60369334",
+    "url": "/js/about.f20e1495.js"
+  },
+  {
+    "revision": "c3b1d421d6693596e4ef2b88c27acca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2da49b1b4d13fdb9a769",
+    "url": "/css/app.ae1365ee.css"
+  }
+];
